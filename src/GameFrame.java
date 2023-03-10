3ffa0009b2173973ae6adb732fb39d249b899909
Jean Gatscha <jean.gatscha@gmail.com>
2023-03-10 22:34:27 +0100

    teset

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class GameFrame extends JFrame { //extends erweitert eine Klasse um die Inhalte einer anderen Klasse
     GameFrame(){
         GamePanel Panel = new GamePanel();
-
         this.add(new GamePanel());
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,8 +7,6 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-    Spieler1 spieler1 = new Spieler1();
-    Spieler2 spieler2 = new Spieler2();
     static final int SCREEN_WIDTH = 1000;
     static final int SCREEN_HEIGHT = 800;
     static final int UNIT_SIZE = 25;
@@ -16,15 +14,20 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS]; //???
     final int y[] = new int[GAME_UNITS];
-    int ROCK_SIZE=50;
+    static final int ROCK_SIZE = 50;
     int rockX = -1;
     int rockY = -1;
     int bodyParts = 6;
+    int bodyParts2 = 6;
     int applesEaten;
+    int applesEaten2;
     int appleX;
+    int appleX2;
     int appleY;
+    int appleY2;
     char direction = 'R';
-    boolean running = false;
+    char direction2 = 'L';
+    boolean isCrawling = false;
     Timer timer;
     Random random;
 
@@ -37,11 +40,8 @@ public class GamePanel extends JPanel implements ActionListener {
         startGame();
     }
     public void startGame() {
-        spieler1.x1[0] = 500;
-        spieler1.y1[0] = 500;
         newApple();
-        running=true;
-        spieler1.running1=true;
+        isCrawling=true;
         timer = new Timer(DELAY,this); //was macht Timer()?; aus welcher Klasse wurde ein Objekt erzeugt?; was bedeutet this in diesem Kontext?
         timer.start();
     }
@@ -51,19 +51,19 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
-        if (running){
+        if (isCrawling){
             g.setColor(Color.black);
             for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
-                    g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-                    g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
             }
             g.setColor(Color.red);
             g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 
             g.setColor(Color.white);
-                if (rockX != -1 && rockY != -1) {
-                    g.fillOval(rockX - ROCK_SIZE/2, rockY - ROCK_SIZE/2, ROCK_SIZE, ROCK_SIZE);
-                }
+            if (rockX != -1 && rockY != -1) {
+                g.fillOval(rockX - ROCK_SIZE/2, rockY - ROCK_SIZE/2, ROCK_SIZE, ROCK_SIZE);
+            }
 
 
             for(int i=0; i<bodyParts; i++) {
@@ -99,12 +99,12 @@ public class GamePanel extends JPanel implements ActionListener {
         appleY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
     }
     public void newRock(){
-      int i = 0;
-            if (rockX == -1 && rockY == -1){
-                rockX = random.nextInt(SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
-                rockY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
-                i++;
-            }
+        int i = 0;
+        if (rockX == -1 && rockY == -1){
+            rockX = random.nextInt(SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
+            rockY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
+            i++;
+        }
     }
 
     public void move(){
@@ -113,24 +113,24 @@ public class GamePanel extends JPanel implements ActionListener {
             y[i] = y[i-1];
         }
         /*Die aktualisierung muss als erstes erfolgen, denn:
-        * x[1] == x[0] bedeutet x1,x0 sind beide an der x Koordinate 0
-        * x[1] == x[0] bedeutet x1,x0 sind beide an der x Koordinate 0
-        * danach wird x[0] erhöht, demnach ist x[0]=25; x[1]=0
-        * würde mann x[0] erst erhöhen wäre x[0]=25, x[1]=25
-        * */
+         * x[1] == x[0] bedeutet x1,x0 sind beide an der x Koordinate 0
+         * x[1] == x[0] bedeutet x1,x0 sind beide an der x Koordinate 0
+         * danach wird x[0] erhöht, demnach ist x[0]=25; x[1]=0
+         * würde mann x[0] erst erhöhen wäre x[0]=25, x[1]=25
+         * */
         switch (direction) {
             case 'U':
                 y[0] = y[0] - UNIT_SIZE;
-            break;
+                break;
             case 'D':
                 y[0] = y[0] + UNIT_SIZE;
-            break;
+                break;
             case 'L':
                 x[0] = x[0] - UNIT_SIZE;
-            break;
+                break;
             case 'R':
                 x[0] = x[0] + UNIT_SIZE; //x[0] ist erst an x=0 dann x=25 usw..
-            break;
+                break;
         }
 
     }
@@ -147,9 +147,9 @@ public class GamePanel extends JPanel implements ActionListener {
         int rockBoxTop = rockY - ROCK_SIZE / 2;
         int rockBoxBottom = rockY + ROCK_SIZE / 2;
 
-                if (x[0] >= rockBoxLeft && x[0] <= rockBoxRight && y[0] >= rockBoxTop && y[0] <= rockBoxBottom) {
-                    running = false;
-                }
+        if (x[0] >= rockBoxLeft && x[0] <= rockBoxRight && y[0] >= rockBoxTop && y[0] <= rockBoxBottom) {
+            isCrawling = false;
+        }
     }
     public void scoreEffects(){
         int i = 2;
@@ -163,26 +163,26 @@ public class GamePanel extends JPanel implements ActionListener {
         for (int i = bodyParts; i>0; i--){ //i=6
             //prüft ob der Kopf die einzelnen Teile der Schlange frisst
             if ((x[0] == x[i])&&(y[0] == y[i])){ //wenn x[0] == x[6] also der Kopf, den letzten Teil der Schlange frisst
-                running = false;
+                isCrawling = false;
             }
         }
         //check if head touches left border
         if (x[0] < 0){
-            running = false;
+            isCrawling = false;
         }
         //check if head touches right border
         if (x[0] > SCREEN_WIDTH){
-            running = false;
+            isCrawling = false;
         }
         //check if head touches top border
         if (y[0] < 0) {
-            running = false;
+            isCrawling = false;
         }
         //check if head touches bottom border
         if (y[0] > SCREEN_HEIGHT) {
-            running = false;
+            isCrawling = false;
         }
-        if (!running){
+        if (!isCrawling){
             timer.stop();
         }
     }
@@ -199,7 +199,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e)  {
-        if (running) {
+        if (isCrawling) {
             move();
             //spieler1.move();
             checkApple();
@@ -210,33 +210,58 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         repaint();
     }
-
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
+            System.out.println(e.getKeyCode());
             switch (e.getKeyCode()){
-                case KeyEvent.VK_LEFT:
+
+                case 65: //left
                     if (direction != 'R'){
                         direction = 'L';
                     }
                     break;
 
-                    case KeyEvent.VK_RIGHT:
-                        if (direction != 'L'){
-                            direction = 'R';
-                        }
+                case 68: //right
+                    if (direction != 'L'){
+                        direction = 'R';
+                    }
                     break;
 
-                    case KeyEvent.VK_UP:
-                        if (direction != 'D'){
-                            direction = 'U';
-                        }
+                case 87: //up
+                    if (direction != 'D'){
+                        direction = 'U';
+                    }
                     break;
 
-                    case KeyEvent.VK_DOWN:
-                        if (direction != 'U'){
-                            direction = 'D';
-                        }
+                case 83: //down
+                    if (direction != 'U'){
+                        direction = 'D';
+                    }
+                    break;
+
+                case 37: //left
+                    if (direction != 'R'){
+                        direction = 'L';
+                    }
+                    break;
+
+                case 39: //right
+                    if (direction != 'L'){
+                        direction = 'R';
+                    }
+                    break;
+
+                case 38: //up
+                    if (direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
+
+                case 40: //down
+                    if (direction != 'U'){
+                        direction = 'D';
+                    }
                     break;
             }
         }
